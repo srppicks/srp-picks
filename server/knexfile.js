d1362c13df9efa108e8f9e9d986236f1293cc95c
Stephan Perreault <srppicks@gmail.com>
2020-06-09 09:16:57 -0400

    Env added

--- server/knexfile.js
+++ server/knexfile.js
@@ -37,19 +37,9 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    ssl: true
   }
 
 };
